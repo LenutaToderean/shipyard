cf2aa51d9b6045f85f60a53b6bb50141b58abbd0
Jonathan Rosado <jonathan.rosado>
2016-05-06 15:26:34 -0400

    - Changed blocking waits in favor of until.visible's

--- scripts/protractor/spec.js
+++ scripts/protractor/spec.js
@@ -5,7 +5,7 @@ var config = {
     imageName: 'alpine',
     tag: 'latest'
 };
-
+// TODO: change selectors to id/model/repeaters
 var sy = {
     usernameInputField: by.model('vm.username'),
     passwordInputField: by.model('vm.password'),
@@ -38,8 +38,8 @@ var sy = {
     createTestProviderMenuTransitioner: by.className('menu transition visible'),
     createTestImagesToTest: by.css('[placeholder="All images"]'),
     createTestImagesToTestCSS: '[placeholder="All images"]',
-    createTestSelectImageToTest: by.css('[data-value="' + config.testName + ':' + config.tag + '"]'),
-    createTestSelectImageToTestCSS: '[data-value="' + config.testName + ':' + config.tag + '"]',
+    createTestSelectImageToTest: by.css('[data-value="' + config.imageName + ':' + config.tag + '"]'),
+    createTestSelectImageToTestCSS: '[data-value="' + config.imageName + ':' + config.tag + '"]',
     createTestSaveButton: by.id('test-create-save-button'),
     editProjectBuildButtons: by.repeater('test in vm.tests'),
     editProjectLoadingMsgNegative: by.css('#content > div.ui.padded.grid.ng-scope > div > div > div.ui.icon.message.negative'),
@@ -52,17 +52,6 @@ var sy = {
     inspectViewTestName: by.id('inspect-view-test-name')
 };
 
-function selectDropdownByNumber( mySelect, optionNum ) {
-    element.findElements(by.tagName('option'))
-        .then(function(options) {
-            options[index].click();
-        });
-};
-
-// TODO: Look into how protractor *knows* when our app is pending tasks. Can we remove the `browser.waits`?
-//             - Protractor may not wait for operations other than http calls. This would explain why blocking sleep()'s drastically improve the success rate of these tests
-// TODO: Look for variant of isElementVisible that actually works. Maybe drop to selenium?
-// TODO: Changes browser.sleep's for browser.wait(protractor.ExpectedConditions.visibilityOf($('#create-test-selectize-label')), 60000);
 // TODO: Let's try to comment each step. Even with descriptive names, it's kind of hard to follow
 
 describe('ILM', function() {
@@ -130,45 +119,49 @@ describe('ILM', function() {
 
     it('should open modal window for create image', function() {
         element(sy.createNewImageButton).click();
-        browser.sleep(2000);
+        // browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createImageModal)), 60000);
         expect(element(sy.createImageModal).isDisplayed()).toBe(true);
         expect(element(sy.createImageHeader).getText()).toEqual('Create Image');
     });
 
     it('should add new image from public registry', function() {
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createImageLocation), 60000));
         element(sy.createImageLocation).click();
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createImageLocationPublicReg), 60000));
         element(sy.createImageLocationPublicReg).click();
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createImageNameSearch), 60000));
         element(sy.createImageNameSearch).click();
-        browser.sleep(2000);
-        element(sy.createImageNameSearch).sendKeys(config.testName);
-        browser.wait(protractor.until.elementLocated(by.className('description')), 60000);
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createImageNameSearch), 60000));
+        element(sy.createImageNameSearch).sendKeys(config.imageName);
+        browser.wait(protractor.until.elementLocated(by.className('description')), 180000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element.all(by.className('description')).get(0), 60000));
         element.all(by.className('description')).get(0).click();
         element(sy.createImageTag).click();
-        browser.wait(protractor.until.elementLocated(by.id('tag-results')), 60000);
-        browser.sleep(2000);
+        browser.wait(protractor.until.elementLocated(by.id('tag-results')), 180000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element.all(by.id('tag-results')).get(0), 60000));
         element.all(by.id('tag-results')).get(0).click();
         element(sy.createImageDescription).sendKeys('image description');
-        browser.wait(protractor.until.elementLocated(sy.createImageSave), 60000);
-        browser.sleep(2000);
+        browser.wait(protractor.until.elementLocated(sy.createImageSave), 180000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createImageSave), 60000));
         element(sy.createImageSave).click();
     });
 
     it('should open the tests modal window', function() {
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createNewTestButton), 60000));
+        browser.wait(protractor.ExpectedConditions.elementToBeClickable(element(sy.createNewTestButton), 60000));
         element(sy.createNewTestButton).click();
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createTestModal), 60000));
         expect(element(sy.createTestModal).isDisplayed()).toBe(true);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createTestHeader), 60000));
         expect(element(sy.createTestHeader).getText()).toEqual('Create Test');
     });
-
+    
     it('should add new test that references the image', function() {
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createTestProviderDropdown), 60000));
         element(sy.createTestProviderDropdown).click();
         browser.wait(protractor.until.elementLocated(sy.createTestProviderMenuTransitioner), 60000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(by.css('div[data-value="Clair [Internal]"]')), 60000));
         $('div[data-value="Clair [Internal]"]').click();
         browser.wait(protractor.ExpectedConditions.visibilityOf($(sy.createTestImagesToTestCSS)), 60000);
         element(sy.createTestImagesToTest).click();
@@ -176,17 +169,17 @@ describe('ILM', function() {
         element(sy.createTestSelectImageToTest).click();
         element(sy.createTestDisplayName).sendKeys(config.testName);
         browser.sleep(2000);
-        element(sy.saveProjectButton).click();
         expect(
             element(sy.createTestDisplayName).getAttribute('value')
         ).toEqual(
             config.testName
         );
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.createTestSaveButton), 60000));
         element(sy.createTestSaveButton).click();
     });
-
+    
     it('should be able to run the test', function() {
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.editProjectBuildButtons.row(0)), 60000));
         // Click the play icon for the test
         element(sy.editProjectBuildButtons.row(0))
             .element(by.css('i[class="play icon"]')).click();
@@ -197,9 +190,9 @@ describe('ILM', function() {
         // Expect the message to have the `negative` class (sice build will fail)
         expect(element(sy.editProjectLoadingMsg).getAttribute('class')).toBe('ui icon message negative');
     });
-
+    
     it('should be able return to project listing via the `Go To Projects` icon', function() {
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(sy.editProjectGoToProjectsButton), 60000));
         // Click the `Go To Projects` button
         element(sy.editProjectGoToProjectsButton).click();
         // Wait for
@@ -207,9 +200,9 @@ describe('ILM', function() {
             .element(by.css('#project-name'));
         browser.wait(protractor.ExpectedConditions.visibilityOf(projectName, 60000));
     });
-
+    
     it('should be able to enter the project"s inspect view', function() {
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element.all(sy.projectListTableOfProjects).get(-1), 60000));
         // Click the `inspect` button for the project
         element.all(sy.projectListTableOfProjects).get(-1)
             .element(by.className('search icon')).click();
@@ -218,16 +211,16 @@ describe('ILM', function() {
         browser.wait(protractor.ExpectedConditions.visibilityOf(inspectHeader, 60000));
         expect(inspectHeader.getText()).toEqual('Project Results');
     });
-
+    
     it('should have the build we just ran', function() {
-        browser.sleep(2000);
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element.all(sy.inspectViewBuilds).get(-1), 60000));
         // Click the `inspect` button for the project
-        element.all(sy.inspectViewBuilds).get(-1)
-            .element(by.className('search icon')).click();
-        // Wait for the inspect view to load and assert success
-        var inspectHeader = element(by.css('.ui.header .content'));
-        browser.wait(protractor.ExpectedConditions.visibilityOf(inspectHeader, 60000));
-        expect(inspectHeader.getText()).toEqual('Project Results');
+        var lastBuild = element.all(sy.inspectViewBuilds).get(-1);
+        expect(
+            lastBuild.element(sy.inspectViewTestName).getText()
+        ).toEqual(
+            config.testName
+        );
     });
 
 });
